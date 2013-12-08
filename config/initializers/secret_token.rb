2bf3ae63daca7494fc86b44732a25103554a1d90
Steven Schaab <steven.schaab@ymail.com>
2013-12-07 21:59:46 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inxsite::Application.config.secret_token = 'a0cc1f803a556fc019e8b4e0b5e6698d4a08a0c4888fa4d7c9a46703edebd74dda7f9dd762a0bbb40724a85cf188a03a875de566451fc1982c3c5d01fc9a9340'
